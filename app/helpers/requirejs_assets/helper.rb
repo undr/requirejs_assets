141837168b325dd712dadb576100a728253e3d1c
undr <undr@yandex.ru>
2012-12-26 10:06:34 +0700

    Added modules configuration for helper

--- app/helpers/requirejs_assets/helper.rb
+++ app/helpers/requirejs_assets/helper.rb
@@ -4,20 +4,38 @@ module RequirejsAssets
     include ActionView::Helpers::JavaScriptHelper
 
     def requirejs_include_tag options={}
-      html = [javascript_include_tag('require.js', data_attributes_hash)]
+      runtime_options = options[:runtime] || {}
+      html = [javascript_include_tag('require.js', data_attributes_hash(options))]
       if requirejs_runtime_options.present?
-        html << javascript_tag("requirejs.config(#{requirejs_runtime_options.deep_merge(options).to_json});")
+        html << javascript_tag("requirejs.config(#{requirejs_runtime_options.deep_merge(runtime_options).to_json});")
       end
       html.join("\n").html_safe
     end
 
+    private
     def requirejs_runtime_options
-      config = Rails.application.config.requirejs.to_hash
+      config = Rails.application.config.requirejs.to_hash.merge(baseUrl: './assets')
+      config.delete(:modules)
       Hash[config.reject{|key, value| value.blank?}]
     end
 
-    def data_attributes_hash
-      {'data-main' => 'application'}
+    def data_attributes_hash options={}
+      hash = {'data-main' => 'application'}
+      modules = available_modules(options[:modules])
+      hash = modules.inject(hash) do |result, module_name|
+        result.merge("data-#{module_name}" => module_name)
+      end if modules.present?
+      hash
+    end
+
+    def available_modules modules
+      return Rails.application.config.requirejs.modules if modules == :all
+      modules = *modules
+      if modules.present?
+        Rails.application.config.requirejs.modules & modules
+      else
+        nil
+      end
     end
   end
 end
--- spec/requirejs_assets/helper_spec.rb
+++ spec/requirejs_assets/helper_spec.rb
@@ -6,7 +6,7 @@ describe RequirejsAssets::Helper do
   describe '#requirejs_include_tag' do
     context 'without options' do
       specify do
-        helper.requirejs_include_tag.should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>"
+        helper.requirejs_include_tag.should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
       end
     end
 
@@ -22,7 +22,7 @@ describe RequirejsAssets::Helper do
       end
 
       specify do
-        helper.requirejs_include_tag.should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"},\"some_lib\":[\"dependency\"]}});\n//]]>\n</script>"
+        helper.requirejs_include_tag.should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"},\"some_lib\":[\"dependency\"]},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
       end
     end
 
@@ -35,7 +35,67 @@ describe RequirejsAssets::Helper do
       end
 
       specify do
-        helper.requirejs_include_tag(shim: {'some_lib' => ['dependency']}).should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"},\"some_lib\":[\"dependency\"]}});\n//]]>\n</script>"
+        helper.requirejs_include_tag(runtime: {shim: {'some_lib' => ['dependency']}}).should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"},\"some_lib\":[\"dependency\"]},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
+      end
+    end
+
+    context 'without modules' do
+      before do
+        Rails.application.config.requirejs.configure do |config|
+          config.modules = ['module1', 'module2']
+        end
+      end
+
+      specify do
+        helper.requirejs_include_tag.should == "<script data-main=\"application\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"}},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
+      end
+    end
+
+    context 'with all modules' do
+      before do
+        Rails.application.config.requirejs.configure do |config|
+          config.modules = ['module1', 'module2']
+        end
+      end
+
+      specify do
+        helper.requirejs_include_tag(modules: :all).should == "<script data-main=\"application\" data-module1=\"module1\" data-module2=\"module2\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"}},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
+      end
+    end
+
+    context 'with modules as string' do
+      before do
+        Rails.application.config.requirejs.configure do |config|
+          config.modules = ['module1', 'module2']
+        end
+      end
+
+      specify do
+        helper.requirejs_include_tag(modules: 'module1').should == "<script data-main=\"application\" data-module1=\"module1\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"}},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
+      end
+    end
+
+    context 'with modules as array' do
+      before do
+        Rails.application.config.requirejs.configure do |config|
+          config.modules = ['module1', 'module2']
+        end
+      end
+
+      specify do
+        helper.requirejs_include_tag(modules: ['module1']).should == "<script data-main=\"application\" data-module1=\"module1\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"}},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
+      end
+    end
+
+    context 'with existing modules' do
+      before do
+        Rails.application.config.requirejs.configure do |config|
+          config.modules = ['module1', 'module2']
+        end
+      end
+
+      specify do
+        helper.requirejs_include_tag(modules: ['module2', 'module3']).should == "<script data-main=\"application\" data-module2=\"module2\" src=\"/assets/require.js\" type=\"text/javascript\"></script>\n<script type=\"text/javascript\">\n//<![CDATA[\nrequirejs.config({\"paths\":{\"module1\":\"prefix/module1\"},\"shim\":{\"underscore\":{\"exports\":\"_\"}},\"baseUrl\":\"./assets\"});\n//]]>\n</script>"
       end
     end
   end
